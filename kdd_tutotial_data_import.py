b5bad89deac9b073652d8b4a37783c5cad5aee9f
Unknown <leo.salemann@me.com>
2018-04-14 13:19:02 -0700

    Reading csv's for both cities, all three data types

--- kdd_tutotial_data_import.py
+++ kdd_tutotial_data_import.py
@@ -6,18 +6,51 @@
 import requests
 import pandas as pd
 
+#-----------------------< BEIJING >----------------------
+
+# Beijing met
 url = 'https://biendata.com/competition/meteorology/bj/2018-04-01-0/2018-04-01-23/2k0d1d8'
 respones= requests.get(url)
-with open ("bj_meteorology_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+with open ("data/Beijing/met/bj_meteorology_2018-04-01-0-2018-04-01-23.csv",'w') as f:
     f.write(respones.text)
 
+# Beijing air
 url = 'https://biendata.com/competition/airquality/bj/2018-04-01-0/2018-04-01-23/2k0d1d8'
 respones= requests.get(url)
-with open ("bj_airquality_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+with open ("data/Beijing/air/bj_airquality_2018-04-01-0-2018-04-01-23.csv",'w') as f:
     f.write(respones.text)
 
-metDf = pd.read_csv('bj_meteorology_2018-04-01-0-2018-04-01-23.csv')
-airDf = pd.read_csv('bj_airquality_2018-04-01-0-2018-04-01-23.csv')
+
+# Beijing grid
+url = 'https://biendata.com/competition/meteorology/bj_grid/2018-04-01-0/2018-04-01-23/2k0d1d8'
+respones= requests.get(url)
+with open ("data/Beijing/grid/bj_grid_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+    f.write(respones.text)
+
+#-----------------------< LONDON >----------------------
+
+# London met
+url = 'https://biendata.com/competition/meteorology/ld/2018-04-01-0/2018-04-01-23/2k0d1d8'
+respones= requests.get(url)
+with open ("data/London/met/ld_meteorology_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+    f.write(respones.text)
+
+# London air
+url = 'https://biendata.com/competition/airquality/ld/2018-04-01-0/2018-04-01-23/2k0d1d8'
+respones= requests.get(url)
+with open ("data/London/air/ld_airquality_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+    f.write(respones.text)
+
+
+# London grid
+url = 'https://biendata.com/competition/meteorology/ld_grid/2018-04-01-0/2018-04-01-23/2k0d1d8'
+respones= requests.get(url)
+with open ("data/London/grid/ld_grid_2018-04-01-0-2018-04-01-23.csv",'w') as f:
+    f.write(respones.text)
+
+
+metDf = pd.read_csv('data/Beijing/met/bj_meteorology_2018-04-01-0-2018-04-01-23.csv')
+airDf = pd.read_csv('data/Beijing/air/bj_airquality_2018-04-01-0-2018-04-01-23.csv')
 
 print(metDf.head())
 print(airDf.head())
