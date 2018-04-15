c2dd09cf6587fd548a869d698a4bda1365690202
Unknown <leo.salemann@me.com>
2018-04-15 01:34:34 -0700

    Code prettymuch works, doing some cleanups.

--- load_data.py
+++ load_data.py
@@ -1,37 +1,39 @@
 
+#!/usr/bin/python
+
 # PURPOSE: Download air quality (air), meterological observatino (met) and meterological grid (grid) 
 #          data for multipe cities and time frames
-#!/usr/bin/python
 #
 # ARGUMENTS: load_data.py <city> <type> <start datetime> <end datetime>
 #
 # WHERE:  <city> is one of Beijing, London, or Both
 #         <type> is one of air, met, grid, all
-#         <start datetime> and <end datetime> yyyy-mm-dd-h
+#         <start datetime> and <end datetime> are in the form yyyy-mm-dd-h
 #
 #
-# EXAMPLES: python load_data.py Beijing air 2018-03-01-0 2018-03-15-14 
+# EXAMPLES: python load_data.py Beijing air 2018-04-01-0 2018-04-15-14 
 #             this will air quality dataload hourl for Beijing between 
-#             March 1, 2018 midnight and March 15, 2018 2pm.
+#             April 1, 2018 midnight and April 15, 2018 2pm.
 #           python load_data.py Both all 2018-03-01-0 2018-03-15-14 
 #             this will load all data for both cities between 
-#             March 1, 2018 midnight and March 15, 2018 2pm.
+#             April 1, 2018 midnight and April 15, 2018 2pm. for all data types
 #
 # based on https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 #
 ##################################################################################
 
 import sys, getopt
+import requests
+
+# FUNCTION load_on_url()
+# Based on https://biendata.com/forum/view_post_category/9
+# # https://biendata.com/competition/airquality/{city}/{start_time}/{end_time}/2k0d1d8
+# https://biendata.com/competition/meteorology/{city}/{start_time}/{end_time}/2k0d1d8
+# https://biendata.com/competition/meteorology/{city}_grid/{start_time}/{end_time}/2k0d1d8
 def load_one_url (city, datatype, from_datetime, to_datetime):
 
-    url = 'https://biendata.com/competition/meteorology/bj/2018-04-01-0/2018-04-01-23/2k0d1d8'
-    url = 'https://biendata.com/competition/airquality/bj/2018-04-01-0/2018-04-01-23/2k0d1d8'
-    url = 'https://biendata.com/competition/meteorology/bj_grid/2018-04-01-0/2018-04-01-23/2k0d1d8'
-    url = 'https://biendata.com/competition/meteorology/ld/2018-04-01-0/2018-04-01-23/2k0d1d8'
-    url = 'https://biendata.com/competition/airquality/ld/2018-04-01-0/2018-04-01-23/2k0d1d8'
-    url = 'https://biendata.com/competition/meteorology/ld_grid/2018-04-01-0/2018-04-01-23/2k0d1d8'
-
     url_preamble = 'https://biendata.com/competition/'
+    url_tail = '/2k0d1d8'
     
     if city == "London":
         url_city = 'ld'
@@ -42,81 +44,38 @@ def load_one_url (city, datatype, from_datetime, to_datetime):
         exit(9999)
 
     if datatype == "air":
-        url_datatype = 'airquality/'
+        url_datatype = 'airquality'
+        city_datapath = url_city + '_' + url_datatype
     elif datatype == "met":
-        url_datatype = 'meteorology/'
+        url_datatype = 'meteorology'
+        city_datapath = url_city + '_' + url_datatype
     elif datatype == "grid":
-        url_datatype = 'meteorology/'
+        url_datatype = 'meteorology'
         url_city = url_city + '_grid'
+        city_datapath = url_city
     else:
         print("*** ERROR: unknown datatype " + datatype)
         exit(9998)
 
-    url_from_datetime = '/' + from_year + '-' + from_month + '-' + from_day + '-' + from_hour
-    url_to_datetime   = '/' + to_year   + '-' + to_month   + '-' + to_day   + '-' + to_hour
-
-    url = url_preamble + url_datatype + url_city + url_from_datetime + url_to_datetime + '/2k0d1d8'
+    url = url_preamble + url_datatype + '/' + url_city + '/' + from_datetime + '/' + to_datetime + url_tail
     print (url)
 
+    outpath = 'data/' + city + '/' + datatype + '/' + city_datapath + '_' + from_datetime + \
+               '_' + to_datetime + '.csv'
+    print (outpath)
+    print ('')
 
-def load_one_datatype(city, datatype, from_datetime, to_datetime):
-    from_year  = int(from_datetime[:4]) #equiv. to left(from_datetime, 4)
-    from_month = int(from_datetime[5:7])
-    from_day   = int(from_datetime[8:10])
-    from_hour  = int(from_datetime[11:len(from_datetime)])
-
-    to_year  = int(to_datetime[:4]) #equiv. to left(to_datetime, 4)
-    to_month = int(to_datetime[5:7])
-    to_day   = int( to_datetime[8:10])
-    to_hour  = int(to_datetime[11:len(to_datetime)])
-
-    year = from_year
-    if (from_year == to_year):
-        last_month = to_month
-    else:
-        last_month = 12
-
-
-
-    if ((from_year == to_year) and (from_month == to_month) and (from_day == to_day)):
-        last_hour = 23
-    else:
-        last_hour = to_hour
-
-    month = from_month
-    day = from_day
-    hour = from_hour
-
-    while year <= int(to_year):
-        print("year: " + str(year))
-        if (year == int(to_year)):
-            last_month = int(to_month)
-        while month <= last_month:
-            print("  month: " + str(month))
-            if ((year == int(to_year)) and (month == int(to_month))):
-                last_day = int(to_day)
-            elif (month == 2):
-                last_day = 28
-            elif (month in {9, 4, 6, 11}):
-                last_day = 30
-            else:
-                last_day = 31
-            
-            #while day <= last_day:
-            #    print (str(year) + "-" + str(month) + "-" + str(day))
-            #    day = day + 1
-            month = month + 1
-        year = year + 1
-        month = 1
-
+    respones= requests.get(url)
+    with open (outpath,'w') as f:
+      f.write(respones.text)
 
 def load_one_city(city, datatype, from_datetime, to_datetime):
    if datatype == "all":
-       load_one_datatype(city, 'air', from_datetime, to_datetime)
-       load_one_datatype(city, 'met', from_datetime, to_datetime)
-       load_one_datatype(city, 'grid', from_datetime, to_datetime)
+       load_one_url(city, 'air', from_datetime, to_datetime)
+       load_one_url(city, 'met', from_datetime, to_datetime)
+       load_one_url(city, 'grid', from_datetime, to_datetime)
    else:
-       load_one_datatype(city, datatype, from_datetime, to_datetime)
+       load_one_url(city, datatype, from_datetime, to_datetime)
 
 def main(argv):
    city = ''
