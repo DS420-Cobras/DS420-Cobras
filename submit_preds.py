4d7e127178c51de953ceb2070c5c131e207547ea
ghand1 <david.p.ghan@gmail.com>
2018-04-16 17:20:04 -0700

    fix typos and arguments

--- submit_preds.py
+++ submit_preds.py
@@ -1,8 +1,8 @@
 import requests
 team_token = ''
 
-def submit_preds(user_id, description, filename)
-	files={'files': open('sample_submission.csv','rb')}
+def submit_preds(csv, user_id, description, filename):
+	files={'files': open(csv,'rb')}
 
 	data = {
 	    "user_id": user_id,
