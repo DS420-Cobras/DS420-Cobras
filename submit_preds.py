a51d635be1df1bea765fca24e5d3d0765ac619cd
ghand1 <david.p.ghan@gmail.com>
2018-04-17 22:41:26 -0700

    remove testing function
    I removed the test function and replaced with the submission function from the KDD2018 website

--- submit_preds.py
+++ submit_preds.py
@@ -3,37 +3,35 @@ import sys
 import getopt
 team_token = ''
 
-# def submit_preds(csv, user_id, description, filename):
-# 	files={'files': open(csv,'rb')}
+def submit_preds(csv, user_id, description, filename):
+	files={'files': open(csv,'rb')}
 
-# 	data = {
-# 	    "user_id": user_id,
-# 	    "team_token": team_token,
-# 	    "description": description,
-# 	    "filename": filename
-# 	}
-# 	url = 'https://biendata.com/competition/kdd_2018_submit/'
-# 	response = requests.post(url, files=files, data=data)
-# 	print(response.text)
-def fun(csv, user_id, description, filename):
-	print(user_id)
-	print(description)
-	print(filename)
+	data = {
+	    "user_id": user_id,
+	    "team_token": team_token,
+	    "description": description,
+	    "filename": filename
+	}
+	url = 'https://biendata.com/competition/kdd_2018_submit/'
+	response = requests.post(url, files=files, data=data)
+	print(response.text)
 
-
-def main(airgv)
-	csv = ''
+def main(argv):
+    csv = ''
     user_id = ''
     from_datetime = ''
     description = ''
     example = 'submit_preds.py -c <"csv"> -u <"user_id"> -d <"description"> -f <"filename">'
     try:
-        opts, args = getopt.getopt(argv,"c:u:d:f:",["csv=","user_id=","description=","filename="])
+        opts, args = getopt.getopt(argv,"hc:u:d:f:",["csv=","user_id=","description=","filename="])
     except getopt.GetoptError:
         print(example)
         sys.exit(2)
     for opt, arg in opts:
-        if opt in ("-c", "--csv"):
+        if opt == '-h':
+            print(example)
+            sys.exit()
+        elif opt in ("-c", "--csv"):
             csv = arg
         elif opt in ("-u", "--user_id"):
             user_id = arg
@@ -41,7 +39,7 @@ def main(airgv)
             description = arg
         elif opt in ("-f", "--filename"):
             filename = arg
-       	fun(csv, user_id, description, filename)
+    submit_preds(csv, user_id, description, filename)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
