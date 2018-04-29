4fc5719bb22dc5d0f0eb485b163cf9a10ad62f0e
ybhandari <ybhandari@users.noreply.github.com>
2018-04-28 18:07:04 -0700

    Use previous values for prediction (#36)
    * Test train split
    
    * Use previous values
    
    * Additional data

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import sklearn.linear_model
 import sklearn.ensemble
 import matplotlib.pyplot as plt
 import plot
+import time
+import datetime
+import numpy as np
 
 allDf = load_data.getPandasDataframes()
 
@@ -27,10 +30,24 @@ bejDf['month'] = bejAirDf['newTime'].dt.month
 #bejDf['dayofweek'] = bejAirDf['newTime'].dt.dayofweek
 #bejDf['dayofweek'] = pd.Categorical(bejDf['dayofweek'], ordered=False)
 
+valueDict = {(row['station_id'],row['time'].to_pydatetime()):row['PM10_Concentration'] for index, row in bejDf.iterrows()}
+bejDf['PM10_3'] = bejDf['PM10_Concentration']
+bejDf['PM10_3B'] = bejDf['PM10_Concentration']
+bejDf['PM10_3C'] = bejDf['PM10_Concentration']
+for index, row in bejDf.iterrows():
+    rtime = row['time'].to_pydatetime()
+    rtime = rtime - datetime.timedelta(hours=1)
+    rtime2 = rtime - datetime.timedelta(hours=1)
+    rtime3 = rtime2 - datetime.timedelta(hours=1)
+    bejDf.at[index, 'PM10_3'] = valueDict.get((row['station_id'], rtime), np.NaN)
+    bejDf.at[index, 'PM10_3B'] = valueDict.get((row['station_id'], rtime2), np.NaN)
+    bejDf.at[index, 'PM10_3C'] = valueDict.get((row['station_id'], rtime3), np.NaN)
+
+
 stationDf = bejDf[bejDf['station_id']=='tiantan_aq']
 
 #ToDo: limit to one station for now
-bejDf = stationDf
+#bejDf = stationDf
 
 if False:
     plt.scatter(x=bejDf.PM25_Concentration, y=bejDf.PM10_Concentration)
@@ -47,17 +64,18 @@ bejDf.dropna(inplace=True)
 # Handle categorical data
 bejDf = pd.get_dummies(bejDf, 'dum')
 
-splitDate = pd.datetime(2018,4,20)
+ct = datetime.datetime.utcnow() - datetime.timedelta(days=3)
+splitDate = pd.datetime(ct.year,ct.month,ct.day,  ct.hour)
 trainDf = bejDf[bejDf['time'] < splitDate]
 testDf = bejDf[bejDf['time'] >= splitDate]
 
 targets = ['PM25_Concentration', 'PM10_Concentration', 'NO2_Concentration', 'CO_Concentration', 'O3_Concentration', 'SO2_Concentration']
 features = [col for col in list(testDf) if ((col not in targets) and (col != 'time'))]
 
-print(testDf[features].head(20))
+print(trainDf[features].head(20))
 
-lm = sklearn.linear_model.LinearRegression(n_jobs=-1)
-#lm = sklearn.ensemble.RandomForestRegressor(n_jobs=-1, random_state=42)
+#lm = sklearn.linear_model.LinearRegression(n_jobs=-1)
+lm = sklearn.ensemble.RandomForestRegressor(n_jobs=-1, random_state=42)
 #lm.fit(trainDf[features], trainDf[targets])
 #print(lm.score(testDf[features], testDf[targets]))
 
