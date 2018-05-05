b822e4e5709d610fe8aa83dd9e4a23f88d4370e6
Yash Bhandari <ybhandari@users.noreply.github.com>
2018-05-05 12:05:38 -0700

    Cleanup of main

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import plot
 import time
 import datetime
 import numpy as np
+import sklearn.preprocessing
 
 allDf = load_data.getPandasDataframes()
 
@@ -35,25 +36,6 @@ bejDf['month'] = bejAirDf['newTime'].dt.month
 bejDf['dayofweek'] = bejAirDf['newTime'].dt.dayofweek
 # bejDf['dayofweek'] = pd.Categorical(bejDf['dayofweek'], ordered=False)
 
-valueDict = {(row['station_id'],row['time'].to_pydatetime()):row['PM10_Concentration'] for index, row in bejDf.iterrows()}
-bejDf['PM10_3'] = bejDf['PM10_Concentration']
-bejDf['PM10_3B'] = bejDf['PM10_Concentration']
-bejDf['PM10_3C'] = bejDf['PM10_Concentration']
-for index, row in bejDf.iterrows():
-    rtime = row['time'].to_pydatetime()
-    rtime = rtime - datetime.timedelta(hours=1)
-    rtime2 = rtime - datetime.timedelta(hours=1)
-    rtime3 = rtime2 - datetime.timedelta(hours=1)
-    bejDf.at[index, 'PM10_3'] = valueDict.get((row['station_id'], rtime), np.NaN)
-    bejDf.at[index, 'PM10_3B'] = valueDict.get((row['station_id'], rtime2), np.NaN)
-    bejDf.at[index, 'PM10_3C'] = valueDict.get((row['station_id'], rtime3), np.NaN)
-
-
-stationDf = bejDf[bejDf['station_id']=='tiantan_aq']
-
-#ToDo: limit to one station for now
-#bejDf = stationDf
-
 if False:
     plt.scatter(x=bejDf.PM25_Concentration, y=bejDf.PM10_Concentration)
     plt.xlabel("time", fontsize=14)
@@ -78,18 +60,23 @@ bj_nn = pd.read_excel('viz/stash/Beijing_Neighbors.xlsx')
 bj_nn = bj_nn.rename(columns={"aq_station": "station_id"})
 bj_nn[bj_nn['met_station']=='fangshan_met'] =  'fangshan_meo'
 
-
 # Join the data
 temp = bejDf.merge(bj_nn, on='station_id', how='left')
 bejWeather = pd.concat([bejMetDf, bejGrd])
 temp = pd.merge(temp, bejWeather,  how='left', on=['met_station','time'])
-temp = temp.drop(columns='id')
+
+# Drop columns not useful in analysis
+temp.drop(labels='id', axis=1, inplace=True)
 
 # Clean some duplicate categorical variables from weather
 temp['weather'].replace('Cloudy','CLOUDY', inplace=True)
 temp['weather'].replace('Sunny/clear','CLEAR_DAY', inplace=True)
 temp['weather'].replace('Rain','RAIN', inplace=True)
+temp['weather'] = temp['weather'].astype('category')
 
+# Create categorical columns from string columns
+temp['met_station'] = temp['met_station'].astype('category')
+temp['station_id'] = temp['station_id'].astype('category')
 
 # Replace some outliers with median values
 temp.loc[temp['humidity']== 999999, 'humidity'] = temp['humidity'].median()
@@ -101,15 +88,32 @@ bj_grid_met_stations = pd.concat([bj_grid_stations, bj_meo_stations.rename(colum
 temp = pd.merge(temp, bj_grid_met_stations, how='left', on = 'met_station')
 
 # remove the NANs. not ideal
-temp_no_null = temp.dropna()
-tempNum = temp_no_null.select_dtypes([np.number])
-tempNum.drop(['latitude','longitude'], axis=1, inplace=True)
-temp_no_null[tempNum.columns]=tempNum.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
-bejDfFinal = pd.get_dummies(temp_no_null, 'dum')
+temp.drop(['latitude','longitude'], axis=1, inplace=True)
+temp.drop(['met_station'], axis=1, inplace=True)
+temp.dropna(inplace=True)
+
+#temp['met_station'] = temp['met_station'].astype('category')
+
+# Normalize the columns
+tempNum = temp.select_dtypes([np.number])
+temp[tempNum.columns]=tempNum.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
 
 # Handle categorical data
-#bejDf = pd.get_dummies(bejDf, 'dum')
+bejDfFinal = pd.get_dummies(temp, 'dum')
 
+"""
+for col in temp.columns:
+    if str(temp[col].dtype) == 'category':
+        continue
+    if str(temp[col].dtype) == 'datetime64[ns]':
+        continue
+    
+    print(col, temp[col].dtype)
+    scaler = sklearn.preprocessing.StandardScaler(copy = True)
+    temp[col] = scaler.fit_transform(temp[col].reshape(-1, 1))
+"""
+
+# Create test and training set
 ct = datetime.datetime.utcnow() - datetime.timedelta(hours=48)
 splitDate = pd.datetime(ct.year,ct.month,ct.day,  ct.hour)
 trainDf = bejDfFinal[bejDfFinal['time'] < splitDate]
@@ -123,8 +127,12 @@ date_list = [pd.to_datetime(i, format='%Y-%m-%d %H:%M:%S') for i in date_list]
 missingDatesFromTestDf = np.setdiff1d(sorted(date_list),list(set(sorted(d))))
 print(list(missingDatesFromTestDf))
 
+# No use of time anymore
+bejDfFinal.drop(labels='time', axis=1, inplace=True)
+
 
 targets = ['PM25_Concentration', 'PM10_Concentration', 'O3_Concentration']
+#targets = ['PM10_Concentration']
 features = [col for col in list(testDf) if ((col not in targets) and (col != 'time'))]
 
 #print(trainDf[features].head(20))
@@ -132,34 +140,32 @@ features = [col for col in list(testDf) if ((col not in targets) and (col != 'ti
 #lm = sklearn.linear_model.LinearRegression(n_jobs=-1)
 lm = sklearn.ensemble.RandomForestRegressor(n_jobs=-1, random_state=42)
 lm.fit(trainDf[features], trainDf[targets])
-#print(lm.score(testDf[features], testDf[targets]))
-
-importances = lm.feature_importances_
-std = np.std([tree.feature_importances_ for tree in lm.estimators_],
-             axis=0)
-indices = np.argsort(importances)[::-1]
-
-# Print the feature ranking
-print("Feature ranking:")
-
-for f in range(trainDf[features].shape[1]):
-    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+print(lm.score(testDf[features], testDf[targets]))
 
 # Plot the feature importances of the random forest model
-plt.figure(figsize=(20,10))
-plt.title("Feature importances")
-plt.bar(range(trainDf[features].shape[1]), importances[indices],
-       color="r", yerr=std[indices], align="center")
-plt.xticks(range(trainDf[features].shape[1]), indices)
-plt.xlim([-1, trainDf[features].shape[1]])
-plt.show()
+if False:
+    importances = lm.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in lm.estimators_],
+             axis=0)
+    indices = np.argsort(importances)[::-1]
 
-#lm.fit(trainDf[features], trainDf['PM10_Concentration'])
-#print(lm.score(testDf[features], testDf['PM10_Concentration']))
+    # Print the feature ranking
+    print("Feature ranking:")
 
-#predictedValues = lm.predict(testDf[features])
-#actualValues = testDf['PM10_Concentration']
-#mn = bejDf['PM10_Concentration'].mean()
-#for i in range(len(predictedValues)):
-#    predictedValues[i] = mn
-#plot.CreateMultiplePredictedAndActualValuesPlots([predictedValues], [actualValues], 'LinearRegression')
+    for f in range(trainDf[features].shape[1]):
+        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+    plt.figure(figsize=(15,10))
+    plt.title("Feature importances")
+    plt.bar(range(trainDf[features].shape[1]), importances[indices],
+           color="r", yerr=std[indices], align="center")
+    plt.xticks(range(trainDf[features].shape[1]), indices)
+    plt.xlim([-1, trainDf[features].shape[1]])
+    plt.show()
+
+if False:
+    #lm.fit(trainDf[features], trainDf['PM10_Concentration'])
+    #print(lm.score(testDf[features], testDf['PM10_Concentration']))
+    predictedValues = lm.predict(testDf[features])
+    actualValues = testDf['PM10_Concentration']
+    plot.CreateMultiplePredictedAndActualValuesPlots([predictedValues], [actualValues], 'Random Forest')
