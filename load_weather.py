e906829c71a91765321f63adf3aaa556071cbc7b
ybhandari <ybhandari@users.noreply.github.com>
2018-05-05 22:08:49 -0700

    Use the new weather API (#47)

--- load_weather.py
+++ load_weather.py
@@ -6,15 +6,20 @@ from pandas.io.json import json_normalize
 from calendar import monthrange
 import os.path
 import datetime
-#function to get weather
-def get_met_data(year, month, day, numdays, api_key, lat, lng, station_id):
+
+#api_key = '276d9b4ae748ec5d42ab2ababe8435cc' #apikey obtained from darksky.net 
+api_key = '999653ec682af395046067847b4f4948' #apikey obtained from darksky.net by Yash
+
+
+def get_met_data(start_date, numdays, api_key, lat, lng, station_id):
+    "Function to get weather"
+
     #get url to retrieve weather information
-    start_date = datetime.datetime(year, month, day)
     date_list = [start_date + datetime.timedelta(days = x) for x in range(0, numdays)]
     hist = np.arange(0, len(date_list)).tolist()
     forecast = []
     for n in hist:
-        forecast.append(forecastio.load_forecast(api_key, lat, lng, time = date_list[n]))
+        forecast.append(forecastio.load_forecast(api_key, lat, lng, date_list[n]))
     #jspn object
     met_data = pd.DataFrame()
     for i in np.arange(0, len(forecast)).tolist():
@@ -26,18 +31,40 @@ def get_met_data(year, month, day, numdays, api_key, lat, lng, station_id):
     met_data['long'] = lng
     met_data['station_id'] = station_id
     return met_data
-#parameters
-api_key = '276d9b4ae748ec5d42ab2ababe8435cc' #apikey obtained from darksky.net 
-start = '2018-04-05' #start day
-numdays = 3 #forecast horizon
-#Cargamos la tabla con las coordenadas
-start_date = datetime.datetime.strptime(start, '%Y-%m-%d')
-#csv with stations, latitude and longitude, and api keys available from 'https://github.com/patarol/kdd2018_stations_list/blob/master/stations_list.csv'
-station_met =  pd.read_csv(r'viz\Beijing_points_mini.csv')
-#call get_met_data function
-forecast_48 = pd.DataFrame()
-for i in np.arange(station_met.shape[0]).tolist():
-    print(station_met['station_id'][i])
-    forecast_48 = pd.concat([forecast_48, get_met_data(start_date.year, start_date.month, start_date.day, numdays, api_key, 
-                                             station_met['latitude'][i], station_met['longitude'][i], station_met['station_id'][i])])
-forecast_48.to_csv(r'viz\Beijing_weather.csv')
+
+def getWeatherDataRange(startDate, endDate, stationsNeeded, cityName, shortRun = True):
+    "Retrieves the weather data from file or from web"
+    assert(startDate<endDate)
+    if os.path.exists('viz\\' + cityName + '_weather.csv'):
+        cached =  pd.read_csv('viz\\' + cityName + '_weather.csv')
+    else:
+        cached = pd.DataFrame()
+    if 'datetime' in set(cached):
+        cached['datetime'] = pd.to_datetime(cached['datetime'], errors='coerce', format='%Y-%m-%d %H:%M:%S')
+
+    datesNeeded = []
+    curDate = startDate
+    while curDate <= endDate:
+        datesNeeded.append(curDate)
+        curDate = curDate + datetime.timedelta(hours=24)
+    count = 0
+    station_met =  pd.read_csv('viz\\' + cityName + '_points.csv')
+    for stations in stationsNeeded:
+        if count == 0 and shortRun:
+            break
+        stationInfo = station_met[station_met['station_id'] == stations]
+        print(str(stationInfo['station_id'].values[0]))
+        for dates in datesNeeded:
+            if count == 3 and shortRun:
+                break
+            flag = True
+            if 'datetime' in set(cached):
+                tempDf = cached[(cached['station_id'] == stations)]
+                tempDf = tempDf[tempDf['datetime'] == dates ]
+                if not tempDf.empty:
+                    flag = False
+            if flag:
+                count += 1
+                cached = pd.concat([cached, get_met_data(dates, 1, api_key, float(stationInfo['latitude']), float(stationInfo['longitude']), str(stationInfo['station_id'].values[0]) )])
+        cached.to_csv('viz\\' + cityName + '_weather.csv', index=False)
+    return cached
